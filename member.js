2dc5c81c6ad2afce17827c8206814bdd2ec6094a
hemanth-arasaiah <50250589+hemanth-arasaiah@users.noreply.github.com>
2025-01-08 08:22:56 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+  return {
+    name: 'John Doe',
+    age: 25,
+    skills: {
+      skill1: 'JavaScript',
+      skill2: 'React',
+      skill3: 'Node.js',
+    },
+  };
+}
